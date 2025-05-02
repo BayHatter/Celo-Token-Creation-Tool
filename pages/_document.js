dc3bf1d1ed3e215824025ef8e489203c71acdf41
Mohsin Khan <209888552+BayHatter@users.noreply.github.com>
2025-05-02 06:22:39 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
-        <title>Ethereum Token Creation Tool</title>
-        <meta name="description" content="Create and launch your own meme token on Ethereum" />
-        <meta property="og:title" content="Launch Your Meme Coin on Ethereum" />
+        <title>Celo Token Creation Tool</title>
+        <meta name="description" content="Create and launch your own meme token on Celo" />
+        <meta property="og:title" content="Launch Your Meme Coin on Celo" />
         <meta property="og:description" content="Build and deploy a meme coin instantly. Fast, secure." />
         <meta property="og:url" content="/" />
         <meta property="og:type" content="website" />
